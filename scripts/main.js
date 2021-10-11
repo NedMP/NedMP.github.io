72719e84f6fcd042d521e0e69c8a481007c239ad
Ned Perkins <nedprks@gmail.com>
2021-10-11 12:03:29 +0000

    Broken until its not

--- scripts/main.js
+++ scripts/main.js
@@ -60,13 +60,13 @@ class App extends React.Component {
 				<section class="container-fluid px-0">
         			<div class="row align-items-center content">
             			<div class="col-md-6 order-2 order-md-1">
-                			<img src="imgs/milk.png" alt="" class="img-fluid">
+                			<img src="imgs/milk.png" alt="" class="img-fluid"></img>
             			</div>
             			<div class="col-md-6 text-center order-1 order-md-2">
                 			<div class="row justify-content-center">
                     			<div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
                         			<h2>MUSEUM OF CANDY</h2>
-                        			<img src="imgs/lolli_icon.png" alt="" class="d-none d-lg-inline">
+                        			<img src="imgs/lolli_icon.png" alt="" class="d-none d-lg-inline"></img>
                         				<p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
                             				beatae, maiores deserunt
                             				in voluptatibus
